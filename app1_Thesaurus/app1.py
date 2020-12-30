a65070e3a8b93447abed3c5ea01bd238ecb77ad0
daniel-ob <danobrey@gmail.com>
2020-12-30 18:58:04 +0100

    Dictionary first version

---
+++ app1_Thesaurus/app1.py
@@ -0,0 +1,32 @@
+import json  # json standard library
+#from difflib import SequenceMatcher
+from difflib import get_close_matches
+
+data_file = "data.json"
+
+with open(data_file) as file:
+    data = json.load(file)
+    # print(type(data))  # <class 'dict'>
+
+
+def get_definition(word):
+    word = word.lower()
+    if word in data:
+        #go first to the condition with higher probability
+        return "\n".join(data[word])  # return one definition per line
+    elif len(get_close_matches(word, data.keys())) > 0:
+        closest_match = get_close_matches(word, data.keys())[0]
+        yn = input("Did you mean %s instead? [Y/N]: " % closest_match)
+        if yn == "Y":
+            return "\n".join(data[closest_match])  # return one definition per line
+        else:
+            return "Word not found"
+    else:
+        return "Word not found"
+
+
+word = input("Enter a word: ")
+
+print(get_definition(word))
+
+
