cbb243566fe856771b83f1ef224f8f12829f362e
daniel-ob <danobrey@gmail.com>
2021-01-11 16:32:01 +0100

    Add app2 Web Map

---
+++ app2_WebMap/map.py
@@ -0,0 +1,44 @@
+import folium
+import pandas
+
+data = pandas.read_csv("data/US_volcanoes.txt")  # Pandas DataFrame
+lat = list(data["LAT"])
+lon = list(data["LON"])
+name = list(data["NAME"])
+elev = list(data["ELEV"])
+
+
+def color_producer(elevation):
+    if elevation < 1500:
+        return 'green'
+    elif 1500 <= elevation <= 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
+basemap = folium.Map(location=[37.02, -24.61], zoom_start=3, tiles="Stamen Terrain")
+
+# Volcanoes Layer
+fgv = folium.FeatureGroup("Volcanoes")
+
+for lt, ln, name, el in zip(lat, lon, name, elev):
+    fgv.add_child(folium.CircleMarker(location=(lt, ln), radius=6, popup=name+"\n"+str(el)+" m",
+                                      fill_color=color_producer(el), color='grey', fill_opacity=0.7))
+
+# World Population Layer
+fgp = folium.FeatureGroup("Population")
+
+fgp.add_child(folium.GeoJson(data=open('data/world.json', 'r', encoding='utf-8-sig').read(),
+                             style_function=lambda x: {'fillColor':
+                                                       'green' if x['properties']['POP2005'] < 10000000
+                                                       else
+                                                       'orange' if 10000000 <= x['properties']['POP2005'] <= 20000000
+                                                       else
+                                                       'red'}))
+
+basemap.add_child(fgv)
+basemap.add_child(fgp)
+basemap.add_child(folium.LayerControl())
+
+basemap.save("Map.html")
