efc0bfd740b3736d95475871d7cd6db37176b62b
daniel-ob <danobrey@gmail.com>
2021-01-12 13:55:50 +0100

    [app3] Add first version of Personal Website

---
+++ app3_PersonalWebsite/app3_Website.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# Homepage
+@app.route("/")
+def home():
+    return render_template("home.html")  # file must be in templates/ folder
+
+
+# About page
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
