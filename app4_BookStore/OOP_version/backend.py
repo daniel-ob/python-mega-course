721db4eee76cd96deb6717f3960595baf8d4f743
daniel-ob <danobrey@gmail.com>
2021-01-22 16:58:09 +0100

    [app4_BookStore] OOP version, improve backend: just one connection opening in __init__ and one closing in __end__. Use class attributes to share variables between methods

--- app4_BookStore/OOP_version/backend.py
+++ app4_BookStore/OOP_version/backend.py
@@ -5,47 +5,36 @@ db_file = "books.db"
 
 class Database:
     def __init__(self):
-        conn = sqlite3.connect(db_file)
-        cur = conn.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS store (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, "
-                    "isbn TEXT)")
-        conn.commit()
-        conn.close()
+        self.conn = sqlite3.connect(db_file)
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS store (id INTEGER PRIMARY KEY, title TEXT, author TEXT, "
+                         "year INTEGER, isbn TEXT)")
+        self.conn.commit()
 
     def view(self):
-        conn = sqlite3.connect(db_file)
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM store")
-        rows = cur.fetchall()  # returns a List of Tuples
-        conn.close()
+        self.cur.execute("SELECT * FROM store")
+        rows = self.cur.fetchall()  # returns a List of Tuples
         return rows
 
     def search(self, title="", author="", year="", isbn=""):
-        conn = sqlite3.connect(db_file)
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM store WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
-        rows = cur.fetchall()  # returns a List of Tuples
-        conn.close()
+        self.cur.execute("SELECT * FROM store WHERE title=? OR author=? OR year=? OR isbn=?",
+                         (title, author, year, isbn))
+        rows = self.cur.fetchall()  # returns a List of Tuples
         return rows
 
     def insert(self, title, author, year, isbn):
-        conn = sqlite3.connect(db_file)
-        cur = conn.cursor()
-        cur.execute("INSERT INTO store VALUES (NULL, ?, ?, ?, ?)", (title, author, year, isbn))
+        self.cur.execute("INSERT INTO store VALUES (NULL, ?, ?, ?, ?)", (title, author, year, isbn))
         # id is an auto-incremented value
-        conn.commit()
-        conn.close()
+        self.conn.commit()
 
     def update(self, id, title, author, year, isbn):
-        conn = sqlite3.connect(db_file)
-        cur = conn.cursor()
-        cur.execute("UPDATE store SET title=?, author=?, year=?, isbn=? WHERE id=?", (title, author, year, isbn, id))
-        conn.commit()
-        conn.close()
+        self.cur.execute("UPDATE store SET title=?, author=?, year=?, isbn=? WHERE id=?",
+                         (title, author, year, isbn, id))
+        self.conn.commit()
 
     def delete(self, id):
-        conn = sqlite3.connect(db_file)
-        cur = conn.cursor()
-        cur.execute("DELETE FROM store WHERE id=?", (id,))
-        conn.commit()
-        conn.close()
+        self.cur.execute("DELETE FROM store WHERE id=?", (id,))
+        self.conn.commit()
+
+    def __end__(self):
+        self.conn.close()
