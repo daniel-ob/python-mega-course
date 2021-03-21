c6d72ac61ad6b3b709b4b3c26e25696226c49905
daniel-ob <danobrey@gmail.com>
2021-03-21 12:15:50 +0100

    [app7_WebScraper] Parse row fields

--- app7_WebScraper/webscraper.py
+++ app7_WebScraper/webscraper.py
@@ -1,3 +1,5 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,4 +12,39 @@ soup = BeautifulSoup(c, "html.parser")
 
 # find all property rows on first results page
 rows = soup.find_all("div", {"class": "propertyRow"})
-print(len(rows))
+# print(len(rows))
+
+# parse property fields
+for row in rows:
+    address = row.find_all("span", {"class": "propAddressCollapse"})[0].text
+
+    area = row.find("span", {"class": "infoSqFt"})
+    area = area.find("b").text if area else None
+
+    beds = row.find("span", {"class": "infoBed"})
+    beds = beds.find("b").text if beds else None
+
+    full_baths = row.find("span", {"class": "infoValueFullBath"})
+    full_baths = full_baths.find("b").text if full_baths else None
+
+    half_baths = row.find("span", {"class": "infoValueHalfBath"})
+    half_baths = half_baths.find("b").text if half_baths else None
+
+    locality = row.find_all("span", {"class": "propAddressCollapse"})[1].text
+
+    lot_size = None
+    for column_group in row.find_all("div", {"class": "columnGroup"}):
+        for feature_group in column_group.find_all("span", {"class": "featureGroup"}):
+            if "Lot Size" in feature_group.text:
+                lot_sizes = column_group.find_all("span", {"class": "featureName"})
+                lot_size = lot_sizes[0].text + lot_sizes[1].text if len(lot_sizes) == 2 else lot_sizes[0].text
+                continue
+
+    mls = row.find("div", {"class": "propertyMLS"}).text
+    mls = re.sub(r'[\n ]', '', mls)
+    mls = re.sub('MLS#', '', mls)
+
+    price = row.find("h4", {"class": "propPrice"}).text
+    price = re.sub(r'[\n ]', '', price)
+
+    print(address, area, beds, full_baths, half_baths, locality, lot_size, mls, price)
