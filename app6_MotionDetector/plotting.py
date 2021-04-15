a3de514cc0d8b2504d1066f82ca07c37d7771aa0
daniel-ob <danobrey@gmail.com>
2021-04-15 22:28:05 +0200

    [app6_MotionDetector] Add hover tooltip to the plot

--- app6_MotionDetector/plotting.py
+++ app6_MotionDetector/plotting.py
@@ -1,9 +1,18 @@
 from bokeh.plotting import figure, output_file, show
 from bokeh.models import FixedTicker
+from bokeh.models import HoverTool, ColumnDataSource
 
 
 def plot(df, file):
-    """Shows a plot of motion-detector log (DataFrame). Also saves this plot in a html file"""
+    """Shows a plot of motion-detector log (DataFrame). Plot is also saved in a html file"""
+
+    # add string version of Enter and Exit columns (use datetime formatting)
+    df["Enter_string"] = df["Enter"].dt.strftime("%Y-%m-%d %H:%M:%S")
+    df["Exit_string"] = df["Exit"].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+    # for HoverTool function we need to convert DataFrame into a ColumnDataSource
+    cds = ColumnDataSource(df)
+
     # create figure object
     p = figure(x_axis_type="datetime", plot_height=300, sizing_mode="stretch_width", title="Motion Detector Graph")
     # remove minor ticks of y axis
@@ -13,8 +22,13 @@ def plot(df, file):
     # hide y grid
     p.ygrid.visible = False
 
-    # add rectangles (for each one: bottom=0, top=1)
-    p.quad(left=df["Enter"], right=df["Exit"], bottom=0, top=1, color="green")
+    # define tooltips label and data
+    hover = HoverTool(tooltips=[("Enter", "@Enter_string"), ("Exit", "@Exit_string")])
+    # add hover to the figure object
+    p.add_tools(hover)
+
+    # add rectangles using cds
+    p.quad(left="Enter", right="Exit", bottom=0, top=1, color="green", source=cds)
 
     # prepare the output file
     output_file(file)
