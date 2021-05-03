a214b5be6eb91d30591e84929c99d9280ceeb3e6
daniel-ob <danobrey@gmail.com>
2021-05-03 21:51:42 +0200

    [app9_DatabaseWebApp] Back-end: Add database model (SQLAlchemy and PostgreSQL)

--- app9_DatabaseWebApp/app.py
+++ app9_DatabaseWebApp/app.py
@@ -1,7 +1,24 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 # Create the application
 app = Flask(__name__)
+# Set URI for the database to be used: dialect[+driver]://user:password@host/dbname
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres123@localhost/height_survey'
+# Create database object for the application
+db = SQLAlchemy(app)
+
+
+class Data(db.Model):
+    """Table to store survey data
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    height = db.Column(db.Integer)
+
+    def __init__(self, email, height):
+        self.email = email
+        self.height = height
 
 
 @app.route("/")  # GET method by default
@@ -24,5 +41,7 @@ def success():
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    # create all tables
+    db.create_all()
+
+    app.run(debug=True)
